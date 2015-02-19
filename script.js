a1e5c590b4a2951f22c76ee6579f5c6d482aa9e8
Kevin Van Kessel <kevin.g.vankessel@gmail.com>
2015-02-19 01:08:18 -0800

    Minor fixes and added functionality

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
 $(document).ready(function() {
+	
+	var startHour = parseInt($("#list_startHour").val());
+	var endHour = parseInt($("#list_endHour").val());
+	
+	//Constrain the start and end hours to sensible values
+	$("#list_startHour").change(function() {
+		startHour = parseInt($(this).val());
+		if(endHour < startHour) {
+			endHour = startHour;
+			$("#list_endHour").val(startHour);
+		}
+	});
+	
+	$("#list_endHour").change(function() {
+		endHour = parseInt($(this).val());
+		if(startHour > endHour) {
+			startHour = endHour;
+			$("#list_startHour").val(endHour);
+		}
+	});
+	
 	//Show or hide the weekend column
 	$("#cb_weekend").change(function() {
 		if(this.checked) {
-			$(".weekend").hide();
-		} else {
 			$(".weekend").show();
+		} else {
+			$(".weekend").hide();
 		}
 	});
+	
+	
 	
 	//Placeholder table filler
 	$(".week-table tr td").html("o");
 });
+
+function hideRow(n) {
+	
+}
+	
+function hideColumn(m) {
+	
+}
+
+//Hide the rows not within the start and end times
+function clipTable() {
+
+}
