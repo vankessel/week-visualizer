7a4597b806fafc3bef8f907c9dd3467172c5b089
Kevin Van Kessel <10148659+vankessel@users.noreply.github.com>
2015-02-18 21:16:28 -0800

    Update

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 	//Show or hide the weekend column
-	$("#cb_weekend").checked = true;
 	$("#cb_weekend").change(function() {
 		if(this.checked) {
 			$(".weekend").hide();
